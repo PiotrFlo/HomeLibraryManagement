dcbe952d105bffbbdbd05b04467a2495b7edca0c
PiotrFlo <15006@eans-nt.edu.pl>
2026-06-30 09:44:16 +0200

    2026-06-30 PF

--- home_library/urls.py
+++ home_library/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manage_books/', include('manage_books.urls')),
+    path('', include('manage_books.urls')),
 ]
